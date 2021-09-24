d0ba9ad508e248b90be32b7ee6c663ac43d5acfa
Kamyab Rostami <kamyabrostamii@gmail.com>
2021-09-24 21:33:58 +0330

    this is commit 1

---
+++ sale/sale/sale/win_login.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace sale
+{
+    /// <summary>
+    /// Interaction logic for win_login.xaml
+    /// </summary>
+    public partial class win_login : Window
+    {
+        public win_login()
+        {
+            InitializeComponent();
+        }
+    }
+}
